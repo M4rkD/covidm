692c428a34d0fd0b4e806526c9800c9e254dc2cd
Mark Dawson <mark.dawson@swansea.ac.uk>
2020-06-23 14:07:31 +0100

    setDTthread to 1 at package load

--- R/covidm_package.R
+++ R/covidm_package.R
@@ -4,6 +4,12 @@
 #' @import lubridate
 NULL
 
-.onUnload <- function (libpath) {
+.onLoad <- function(libname, pkgname) {
+  # Data table should be single threaded
+  packageStartupMessage("Setting data table to single threaded...")
+  setDTthreads(1)
+}
+
+.onUnload <- function(libpath) {
   library.dynam.unload("covidm", libpath)
 }
